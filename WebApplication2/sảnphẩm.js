e3fe90a10e70aea5350d90370498598f0fdcb8a2
CH3COOCH2CH2CH3 <m134gold@gmail.com>
2024-10-02 16:23:45 +0700

    Add project files.

---
+++ WebApplication2/sảnphẩm.js
@@ -0,0 +1,19 @@
+﻿document.querySelector('.product-link').addEventListener('mouseover', function () {
+    document.getElementById('productMenu').style.display = 'block';
+});
+
+document.querySelector('.product-link').addEventListener('mouseout', function () {
+    setTimeout(function () {
+        if (!document.querySelector('.product-menu:hover')) {
+            document.getElementById('productMenu').style.display = 'none';
+        }
+    }, 200);
+});
+
+document.getElementById('productMenu').addEventListener('mouseover', function () {
+    this.style.display = 'block';
+});
+
+document.getElementById('productMenu').addEventListener('mouseout', function () {
+    this.style.display = 'none';
+});
